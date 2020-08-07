b922c919cadfb52392b082a79e30eba43e162b52
weboloper <61120299+weboloper-dbs@users.noreply.github.com>
2020-08-07 22:49:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+
+// random Number1
+var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+
+// RANDOM images
+
+var randomImage1 = "images/dice" + randomNumber1 + ".png" ;
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImage1) ;
+
+// 2nd PART
+
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+var randomImage2 = "images/dice" + randomNumber2+ ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImage2) ;
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML =  "🚩Player 1 Wins";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML =  "Player 2 Wins🚩";
+} else {
+  document.querySelector("h1").innerHTML =  "😂DRAW😂";
+}
